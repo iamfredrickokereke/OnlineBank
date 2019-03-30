a033e0423a33fd157e6460143e2df9c50ba2d28b
Okoye emmanuel <chuks.okoye@tenece.com>
2019-03-30 16:29:08 +0100

    initial commit for our atm

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ATM
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "CustomRoute",
+                url: "Atm/{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "profile", id = UrlParameter.Optional }
+                 );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ATM.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+         [HttpGet]
+        public ActionResult Index()
+        {
+     
+            return View();
+        }
+        [HttpPost]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Event()
+        {
+            //ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Event(string testing)
+        {
+            ViewBag.Message = testing;
+            return View() ;
+        }
+
+    }
+}
